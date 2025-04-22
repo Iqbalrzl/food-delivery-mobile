f520d6c0ad1e6f1b4b6b668ab11d80180428303d
Iqbaal <iqbaalrizaaldy1@gmail.com>
2025-04-22 22:31:16 +0700

    Add Order History & Profile Pages

--- lib/pages/order_history_page.dart
+++ lib/pages/order_history_page.dart
@@ -5,6 +5,6 @@ class OrderHistoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(appBar: AppBar(title: Text("Order History")));
   }
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_mobile/auth/login_or_register.dart';
-import 'package:food_delivery_mobile/components/menu_tile.dart';
 import 'package:food_delivery_mobile/components/my_button.dart';
-import 'package:food_delivery_mobile/components/my_drawer_tile.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -15,37 +13,41 @@ class _MyWidgetState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: CircleAvatar(
-              backgroundImage: AssetImage("assets/images/avatar-default.jpg"),
-              radius: 100,
+      appBar: AppBar(scrolledUnderElevation: 0),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/avatar-default.jpg"),
+                radius: 100,
+              ),
             ),
-          ),
 
-          SizedBox(height: 25),
+            SizedBox(height: 25),
 
-          Center(child: Text("Iqal Mahendra", style: TextStyle(fontSize: 25))),
+            Center(
+              child: Text("Iqal Mahendra", style: TextStyle(fontSize: 25)),
+            ),
 
-          Spacer(),
+            SizedBox(height: 470),
 
-          MyButton(
-            onTap: () {
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => LoginOrRegister()),
-                (route) => false,
-              );
-            },
-            text: "Logout",
-          ),
+            MyButton(
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginOrRegister()),
+                  (route) => false,
+                );
+              },
+              text: "Logout",
+            ),
 
-          SizedBox(height: 80),
-        ],
+            SizedBox(height: 80),
+          ],
+        ),
       ),
     );
   }
